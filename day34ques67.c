c0913b2c97b75ad4dfc2f8248342e6c1847f5207
Onus gaba <onusgaba19@gmail.com>
2026-09-12 22:47:59 +0530

    Add files via upload

---
+++ day34ques67.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[100], n, pos, element, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements: ");
+    for(i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    printf("Enter position: ");
+    scanf("%d", &pos);
+
+    printf("Enter element: ");
+    scanf("%d", &element);
+
+    for(i = n; i >= pos; i--)
+        a[i] = a[i - 1];
+
+    a[pos - 1] = element;
+    n++;
+
+    printf("Array after insertion: ");
+    for(i = 0; i < n; i++)
+        printf("%d ", a[i]);
+
+    return 0;
+}
